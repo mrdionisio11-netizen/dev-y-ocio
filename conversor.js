7460a7f308a26a18ee815d97c67e1b9388997288
mrdionisio11-netizen <mrdionisio11@gmail.com>
2026-05-06 13:06:33 +0200

    Agrego conversor euro-dolar

---
+++ conversor.js
@@ -0,0 +1,10 @@
+/*function conversionEuro (a){
+    return a * 1.17; // Ejemplo de conversión de euros a dólares    
+}
+
+console.log(conversionEuro(10)); // Convierte 10 euros a dólares usando una tasa de cambio de 1.17
+*/
+const conversionEuro = (a) => a * 1.17; // Ejemplo de conversión de euros a dólares usando una función flecha
+
+const conversionDolar = (a) => a * 0.85; // Ejemplo de conversión de dólares a euros usando una función flecha
+
